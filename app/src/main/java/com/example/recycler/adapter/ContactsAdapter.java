525370e6e6c4b03cf74f4c65e32a396623269cd5
Softwarica College Lab <softwarica@softwarica.com>
2019-04-18 11:29:25 +0545

    added viewholder in contactsView Holder java class

--- app/src/main/java/com/example/recycler/adapter/ContactsAdapter.java
+++ app/src/main/java/com/example/recycler/adapter/ContactsAdapter.java
@@ -1,5 +1,25 @@
 package com.example.recycler.adapter;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.recycler.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ContactsAdapter {
 
+    public class ContactsViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView imgProfile;
+        TextView tvName, tvPhone;
+        public ContactsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgProfile = itemView.findViewById(R.id.imgProfile);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvPhone = itemView.findViewById(R.id.tvPhone);
+        }
+    }
 }
