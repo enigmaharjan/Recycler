3b904b32afaf443e3fd1f7a3e5248c9177a999c1
Softwarica College Lab <softwarica@softwarica.com>
2019-04-18 11:48:17 +0545

    added code in contactsView Holder java class

--- app/src/main/java/com/example/recycler/adapter/ContactsAdapter.java
+++ app/src/main/java/com/example/recycler/adapter/ContactsAdapter.java
@@ -1,15 +1,44 @@
 package com.example.recycler.adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.recycler.R;
+import com.example.recycler.model.Contacts;
+
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class ContactsAdapter {
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder>{
+
+    Context mContext;
+    List<Contacts> contactsList;
+
+    public ContactsAdapter(Context mContext, List<Contacts> contactsList) {
+        this.mContext = mContext;
+        this.contactsList = contactsList;
+    }
+
+    @NonNull
+    @Override
+    public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsViewHolder contactsViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public class ContactsViewHolder extends RecyclerView.ViewHolder {
 
