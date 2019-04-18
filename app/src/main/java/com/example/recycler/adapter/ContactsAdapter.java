848c4c9e4d1798f0411c8afed7cbc7d2cac437b6
Softwarica College Lab <softwarica@softwarica.com>
2019-04-18 12:00:45 +0545

    done

--- app/src/main/java/com/example/recycler/adapter/ContactsAdapter.java
+++ app/src/main/java/com/example/recycler/adapter/ContactsAdapter.java
@@ -3,6 +3,7 @@ package com.example.recycler.adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -27,17 +28,24 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.Contac
     @NonNull
     @Override
     public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.contact, viewGroup, false);
+        return new ContactsViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ContactsViewHolder contactsViewHolder, int i) {
 
+        Contacts contacts = contactsList.get(i);
+        contactsViewHolder.imgProfile.setImageResource(contacts.getImageId());
+        contactsViewHolder.tvName.setText(contacts.getName());
+        contactsViewHolder.tvPhone.setText(contacts.getPhoneNo());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return contactsList.size();
     }
 
     public class ContactsViewHolder extends RecyclerView.ViewHolder {
